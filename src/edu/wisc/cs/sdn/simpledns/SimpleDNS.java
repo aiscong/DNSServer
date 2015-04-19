6d894316eb48c94149b89a706c185fa4a12f8051
Cong <aiscong@gmail.com>
2015-04-18 20:21:05 -0500

    ec2 file reader

--- src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
+++ src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
@@ -1,6 +1,6 @@
 package edu.wisc.cs.sdn.simpledns;
 import java.util.*;
-import java.io.IOException;
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
@@ -20,6 +20,7 @@ public class SimpleDNS {
 	
 	public static void main(String[] args){
         System.out.println("Hello, DNS!"); 
+        //EC2Entry.printBinary(EC2Entry.slashToMask(31));
         //java edu.wisc.cs.sdn.simpledns.SimpleDNS -r <root server ip> -e <ec2 csv>
         //server should listen for UDP packets on port 8053
         if(args.length != 4){
@@ -68,7 +69,54 @@ public class SimpleDNS {
 		DNSQuestion que = query.getQuestions().get(0);
 		if(!validType.contains(que.getType())) return;
 		System.out.println("Query type is " + que.getType());
-		
-		
+	}
+	
+	/**
+	 * Read EC2 file
+	 */
+	public static List<EC2Entry> readEC2File(String fileName){
+		List<EC2Entry> result = new ArrayList<EC2Entry>();
+		BufferedReader in;
+		try {
+			in = new BufferedReader(new FileReader(fileName));
+			while(in.ready()){
+			String line = in.readLine();
+			String ipDot = null;
+			String region = null;
+			int slash = -1;
+			int ip = 0;
+			int i;
+			for(i = 0; i < line.length(); i++){
+				if(line.charAt(i) == '/'){
+					ipDot = line.substring(0, i);
+					ip = EC2Entry.dotToInt(ipDot);
+					break;
+				}
+			}
+			i++;
+			String[] splitline = line.substring(i, line.length()).split(",");
+			if(splitline == null){
+				System.out.println("parse error split line");
+				System.exit(-1);
+			}
+			if(splitline.length != 2){
+				System.out.println("parse error split line");
+				System.exit(-1);
+			}
+			
+			slash = Integer.valueOf(splitline[0]);
+			if(slash != -1){
+				slash = EC2Entry.slashToMask(slash);
+			}
+			region = splitline[2];
+			result.add(new EC2Entry(slash, ip, region));
+			}
+			in.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		return result;
 	}
 }
